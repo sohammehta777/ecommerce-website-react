041db7695699eb5aa2604d7b7325cf44ba759e44
Guts <sohammehta62@gmail.com>
2023-05-24 17:13:42 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,237 @@
+
+export const storeProducts = [
+  {
+      "id": "330-000010",
+      "title": "Initial Consultation",
+      "category": "Consultation",
+      "info": "Comprehensive Ayurvedic Health Plan to address your health goals (For New Clients)",
+      "price": "195",
+      "availability": "Available",
+      "duration": "1:29:59",
+      "img": "https://nitya-images.s3.us-west-1.amazonaws.com/treatments/Heath%2Bconsultation.jpg",
+      "treatment_notes": "Comprehensive Ayurvedic health plan to address your health goals. In this 90 minute consultation, we cover your current health concerns in detail, identify any causative factors, and create a personalized treatment plan that includes diet, lifestyle, herbs and therapy suggestions. Herbs are not included. Online consultation is available for those clients not in the San Jose area.",
+      "display_order": 1
+  },
+  {
+      "id": "330-000005",
+      "title": "Follow Up Appointment",
+      "category": "Consultation",
+      "info": "For Returning Clients to fine tune their Ayurvedic health plan",
+      "price": "80",
+      "availability": "Available",
+      "duration": "0:44:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Returning_Client.webp",
+      "treatment_notes": "Herbs are not included.Online consultation is available for those clients not in the San Jose area.",
+      "display_order": 3
+  },
+  {
+      "id": "330-000006",
+      "title": "Monthly Follow Up Package",
+      "category": "Consultation",
+      "info": "Monthly support and guidance for established clients to maintain health goals",
+      "price": "180 for 3 Sessions",
+      "availability": "Available",
+      "duration": "0:44:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Support%26HealthMaintanence.webp",
+      "treatment_notes": "This plan is designed to help you achieve and maintain your health goals with steady support and guidance. As an established client, you meet the Ayurvedic Practitioner monthly to discuss any changes in your health and update your Ayurvedic treatment plan.Herbs are not included in this plan. Online consultation is available for those clients not in the San Jose area.",
+      "display_order": 4
+  },
+  {
+      "id": "330-000001",
+      "title": "Abhyanga",
+      "category": "Treatment",
+      "info": "Nourishing and strengthening the body with oil application.",
+      "price": "175",
+      "availability": "Available",
+      "duration": "0:59:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Abhyanga.webp",
+      "treatment_notes": "Abhyanga is recommended as a daily routine to maintain health, delay the aging process and promote good sleep. It is a traditional Ayurvedic treatment that involves full body oil application with a gentle and soothing massage.",
+      "display_order": 1
+  },
+  {
+      "id": "330-000012",
+      "title": "Abhyanga and Full-Body Steam",
+      "category": "Treatment",
+      "info": "Nourishing and strengthening the body with oil application followed by a full-body steam",
+      "price": "195",
+      "availability": "Available",
+      "duration": "1:14:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Abhyanga.webp",
+      "treatment_notes": "Abhyanga is recommended as a daily routine to maintain health, delay the aging process and promote good sleep. It is a traditional Ayurvedic treatment that involves full body oil application with a gentle and soothing massage. Abhyanga is followed by a full body steam for 10 minutes. (Steam may not be recommended for all clients)",
+      "display_order": 2
+  },
+  {
+      "id": "330-000008",
+      "title": "Shirodhara",
+      "category": "Treatment",
+      "info": "Warm medicated oil drip on the forehead",
+      "price": "175",
+      "availability": "Available",
+      "duration": "0:59:59",
+      "img": "https://s3-us-west-1.amazonaws.com/nitya-images/treatments/None_2022_04_17_22_13_15",
+      "treatment_notes": "Shirodhara session starts with a relaxing head, neck, and foot massage. After you relax completely, warm medicated oil is dripped gently on the forehead for ~15-20 mins. The warm oil calms the nervous system, relieves anxiety and induces good sleep.",
+      "display_order": 3
+  },
+  {
+      "id": "330-000002",
+      "title": "Kati Basti",
+      "category": "Treatment",
+      "info": "Nourishing treatment for lower back pain.",
+      "price": "175",
+      "availability": "Available",
+      "duration": "0:59:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Kati+Basti.webp",
+      "treatment_notes": "This treatment begins with a relaxing full body massage, followed by an application of a soothing oil bath to the lower back. A dough ring is applied to the lower back to create a localized warm oil bath to ease the pain. This treatment eases the muscles and tendons in the lower back and provides nourishment that can reach deep into the bone.",
+      "display_order": 4
+  },
+  {
+      "id": "330-000003",
+      "title": "Hrud Basti",
+      "category": "Treatment",
+      "info": "Treatment for nourishing and opening the heart chakra.",
+      "price": "175",
+      "availability": "Available",
+      "duration": "0:59:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Hrud+Basti.webp",
+      "treatment_notes": "Hrud Basti treatment begins with a gentle full body oil massage followed by an application of a dough ring to create a localized pool of warm oil at the heart region. In Ayurveda, mind is said to reside at the heart. This treatment not only opens the heart chakra but also has a deep nourishing and healing effect on the mind.",
+      "display_order": 5
+  },
+  {
+      "id": "330-000011",
+      "title": "Test Treatment Session",
+      "category": "Treatment",
+      "info": "Detailed information about Treatment",
+      "price": "1",
+      "availability": "Available",
+      "duration": "0:29:59",
+      "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Support%26HealthMaintanence.webp",
+      "treatment_notes": "Not a valid service - for test purposes only",
+      "display_order": 7
+  }
+]
+
+export const detailProduct =   {
+  "id": "330-000011",
+  "title": "Test Treatment Session",
+  "category": "Treatment",
+  "info": "Detailed information about Treatment",
+  "price": "1",
+  "availability": "Available",
+  "duration": "0:29:59",
+  "img": "https://nitya-images.s3-us-west-1.amazonaws.com/treatments/Support%26HealthMaintanence.webp",
+  "treatment_notes": "Not a valid service - for test purposes only",
+  "display_order": 7
+};
+
+
+// export const storeProducts = [
+//   {
+//     id: 1,
+//     title: "Google Pixel - Black",
+//     img: "img/product-1.png",
+//     price: 10,
+//     company: "GOOGLE",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 2,
+//     title: "Samsung S7",
+//     img: "img/product-2.png",
+//     price: 16,
+//     company: "SAMSUNG",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 3,
+//     title: "HTC 10 - Black",
+//     img: "img/product-3.png",
+//     price: 8,
+//     company: "htc",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 4,
+//     title: "HTC 10 - White",
+//     img: "img/product-4.png",
+//     price: 18,
+//     company: "htc",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 5,
+//     title: "HTC Desire 626s",
+//     img: "img/product-5.png",
+//     price: 24,
+//     company: "htc",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 6,
+//     title: "Vintage Iphone",
+//     img: "img/product-6.png",
+//     price: 17,
+//     company: "apple",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 7,
+//     title: "Iphone 7",
+//     img: "img/product-7.png",
+//     price: 30,
+//     company: "apple",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   },
+//   {
+//     id: 8,
+//     title: "Smashed Iphone",
+//     img: "img/product-8.png",
+//     price: 2,
+//     company: "apple",
+//     info:
+//       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//     inCart: false,
+//     count: 0,
+//     total: 0
+//   }
+// ];
+
+// export const detailProduct = {
+//   id: 1,
+//   title: "Google Pixel - Black",
+//   img: "img/product-1.png",
+//   price: 10,
+//   company: "google",
+//   info:
+//     "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+//   inCart: false,
+//   count: 0,
+//   total: 0
+// };
